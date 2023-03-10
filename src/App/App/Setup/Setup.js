e67d51a72b84f59e9f748aa6ce3c5ae839e6e604
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2023-03-10 18:03:29 +1000

    yay

--- src/App/App/Setup/Setup.js
+++ src/App/App/Setup/Setup.js
@@ -30,6 +30,8 @@ firebase.initializeApp({
 const auth = firebase.auth();
 
 export function Setup() {
+    const [step, setStep] = useState(0);
+
     const [user] = useAuthState(auth);
     const navigate = useNavigate();
 
@@ -38,7 +40,36 @@ export function Setup() {
             <div class="setup-container">
                 <div className="setup-main">
                     <div className="setup-content">
-
+                        {step === 0 ? (
+                            <>
+                                <div>
+                                    <h1>Let's Get Started</h1>
+                                    <p>
+                                        Welcome to the CampusRoute setup page!
+                                        We're excited to join you on your
+                                        university journey and provide you with
+                                        a seamless navigation and organization
+                                        experience. By setting up CampusRoute,
+                                        you'll have access to real-time
+                                        directions to classes, reminders for
+                                        homework and assignments, and class
+                                        transition notifications. With our
+                                        user-friendly interface and helpful
+                                        features, you can focus on what's
+                                        important - your academic goals.
+                                    </p>
+                                </div>
+                                <button
+                                    onClick={() => {
+                                        setStep(step + 1);
+                                    }}
+                                >
+                                    Got it!
+                                </button>
+                            </>
+                        ) : (
+                            <p>ok</p>
+                        )}
                     </div>
                     <div className="setup-photo">
                         <img src={infoSchool} alt="" />
--- src/App/Home/Components/Footer.js
+++ src/App/Home/Components/Footer.js
@@ -5,32 +5,55 @@ import "../Home.css";
 import logo from "../../../Assets/logo.svg";
 import logoBig from "../../../Assets/logoBig.svg";
 
+import brandGithub from "../../../Assets/brands/github.svg"
+
 export function Footer() {
     return (
-        <footer className="footer">
-            <div className="footer-container">
-                <div className="footer-container-content">
-                    <div>
-                        <img src={logoBig} alt="logo" className="footer-logo" />
-                    </div>
-                    <div className="footer-item-container">
-                        <div className="footer-item">
-                            <h1>Made by:</h1>
-                            <h3><a href="https://github.com/what-question-mark" target="_blank">Jack Weller</a></h3>
+        <>
+            <footer className="footer">
+                <div className="footer-container">
+                    <div className="footer-container-content">
+                        <div>
+                            <img
+                                src={logoBig}
+                                alt="logo"
+                                className="footer-logo"
+                            />
+                        </div>
+                        <div className="footer-item-container">
+                            <div className="footer-item">
+                                <h1>Made by:</h1>
+                                <h3>
+                                    <a
+                                        href="https://github.com/what-question-mark"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Jack Weller
+                                    </a>
+                                </h3>
+                            </div>
+                            <div className="footer-item"></div>
+                            <div className="footer-item">
+                                <ul>
+                                    <a href="https://github.com/What-Question-Mark/campusRoute" target="_blank" rel="noreferrer"><img src={brandGithub} /></a>
+                                </ul>
+                            </div>
                         </div>
                     </div>
+                    <div className="footer-acknowledgement">
+                        <p className="footer-acknowledgement-content">
+                            We acknowledge the Traditional Owners of the land
+                            where we work and live. We pay our respects to
+                            Elders past, present and emerging. We celebrate the
+                            stories, culture and traditions of Aboriginal and
+                            Torres Strait Islander Elders of all communities who
+                            also work and live on this land.
+                        </p>
+                    </div>
                 </div>
-                <div className="footer-acknowledgement">
-                    <p className="footer-acknowledgement-content">
-                        We acknowledge the Traditional Owners of the land where
-                        we work and live. We pay our respects to Elders past,
-                        present and emerging. We celebrate the stories, culture
-                        and traditions of Aboriginal and Torres Strait Islander
-                        Elders of all communities who also work and live on this
-                        land.
-                    </p>
-                </div>
-            </div>
-        </footer>
+            </footer>
+            <div className="footer-gradient"></div>
+        </>
     );
 }
