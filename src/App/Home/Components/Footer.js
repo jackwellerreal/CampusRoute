52a5ad95647410f5946e01e0a81b3544822b110a
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2023-04-04 09:22:59 +1000

    Changed url

--- src/App/Home/Components/Footer.js
+++ src/App/Home/Components/Footer.js
@@ -29,7 +29,7 @@ export function Footer() {
                                         <a href="../plus">NavigatorPlus</a>
                                     </li>
                                     <li>
-                                        <a href="https://stats.uptimerobot.com/NjGPNIZKWp">Status</a>
+                                        <a href="https://stats.uptimerobot.com/2rX63cVqwO">Status</a>
                                     </li>
                                 </ul>
                             </div>
